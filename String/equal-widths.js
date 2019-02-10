9df157e873bfcd6295b687ec430546ac49f50141
tmarshall <timothyjmarshall@gmail.com>
2019-02-10 13:20:43 -0800

    +String.equal-widths

---
+++ String/equal-widths.js
@@ -0,0 +1,15 @@
+// takes in an array of strings,
+// and returns a modified version
+// where all values have been padded
+// to equal the longest's,
+// plus extra amount as specified
+function equalWidths(strings, extraPadding = 0) {
+  const longestLen = strings.reduce((longestLen, str) => {
+    if (str.length > longestLen) {
+      return str.length
+    }
+    return longestLen
+  }, 0)
+
+  return strings.map(str => str + ' '.repeat(longestLen - str.length + extraPadding))
+}
